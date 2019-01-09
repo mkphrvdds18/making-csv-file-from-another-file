e45f53adca6a0cd0d9e008610ce1319d3b857aaf
mkphrvdds18 <29808555+mkphrvdds18@users.noreply.github.com>
2019-01-09 14:12:07 -0500

    Update df_update.py

--- df_update.py
+++ df_update.py
@@ -1,15 +1,23 @@
+#below will make a new dataframe my_df using informatiom another dataframe name as df
+# , assuming df has 60 lines 
+# next for will scan df dataframe and look for a match of the company name from new_list
+# when there is a match , it will gather some info and make a new data frame
+# many lines are skipped , that is for readers for evaluation , such as opening csv file , writing to new CSV file
 
-
-with open("ins_list.txt", "r") as ins:
+# txt file has company name , 1 per line
+with open("co_list.txt", "r") as ins:
     lines = ins.read().split("\n")
 new_list=[]
 for items in lines:
     if items != '':
         new_list.append(items)
-print("Total number of insurance companies:", len(new_list))
-#print top 5 names
-print(new_list[0:3])
+print("Total number of companies:", len(new_list))
+#print few names, for test only
+print(new_list[0:4])
 
+
+# info in df: df has 2 cols  , 1st col: col_object, 2nd col : col_value, both may have missing values, 
+# however, row locations for contact, email , web etc. are all in relative position from the row position of company name
 
 for m in [0,1,2,3,4]:
     for i in range(0, 60):
@@ -25,3 +33,6 @@ for m in [0,1,2,3,4]:
             my_df.loc[len(my_df)] = [company, contact, email, web, phone]
             print("appended")
 			
+#test 
+print(my_df)
+#output will show an xcel style output with 1st row as : company-contact-email-web-phone
